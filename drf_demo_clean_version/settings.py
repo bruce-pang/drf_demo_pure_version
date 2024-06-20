94851c8f6cd50df1a9956102812c5982b618c1fb
BrucePang <brucepang0618@gmail.com>
2024-06-20 14:33:55 +0800

    [init] init drf_demo_clean_version in current directory

--- drf_demo_clean_version/settings.py
+++ drf_demo_clean_version/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)qyeb^2+%z(w%a3(bgyf$cenk*lk1pgn!qwc=+9j+skmgds6$y'
+SECRET_KEY = 'django-insecure-is%pj_7m_m%%7tyz9v8@^5!4tvbypup%t8u(=e_g6#arsn83+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
