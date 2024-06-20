8f78fdc9aafefe358759374c09333b7ad0b9fb14
BrucePang <brucepang0618@gmail.com>
2024-06-20 14:57:38 +0800

    [feature] add demo to appear contenttype error

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.request import Request
 
-# Create your views here.
+
+class UserView(APIView):
+    def get(self, request):
+        # print(request.user) # 当前登录的用户 -> 匿名用户（源码DRF.request-> _not_authenticated）
+        # print(request.auth)
+        return Response("UserView get")
